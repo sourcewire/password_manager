8acc4f14d0c9a94f185fa3154b744b02ca2621d3
sourcewire <cyberskis@proton.me>
2024-02-10 19:45:25 -0800

    added create login and working on search logins

--- password_manager.py
+++ password_manager.py
@@ -49,11 +49,37 @@ def password_manager():
                     print("\n")
                     if user_choice2 == '1':
                         #create login
-                        print('block')
+                        website = input("Enter the website name: ")
+
+                        username = input("Enter your username: ")
+
+                        note = input("Enter a note (optional): ")
+
+                        create_login(website, username, note)
+                        print("Login added sucessfully")
 
                     elif user_choice2 == '2':
                         #search logins
-                        print('block')
+                        print("1.Search by website name")
+                        print("2.Search by username")
+                        print("3.Exit")
+
+                        user_choice3 = input("Choose an option from above: ")
+
+                        if user_choice3 == '1':
+                            entered_website = input("Enter the name of the website you want to find login information for: ")
+                            
+                            find_login_by_website(entered_website)
+
+                        elif user_choice == '2':
+                            entered_username = input("Enter the username you want to search for log in information for: ")
+
+                            find_login_by_username(entered_username)
+
+                        elif user_choice == '3':
+                            sys.exit()
+
+
 
                     elif user_choice2 == '3':
                         #update login
@@ -108,33 +134,37 @@ def login(entered_password):
 
 
 
+#create new login -- not adding password functionality yet because it requires microservice
+def create_login(website, username, note):
+    if not os.path.exists('logins.json'):
+        #if logins.json doesnt exit init it with empty list
+        logins = []
+
+    else:
+        #load logins from logins.json
+        try:
+            with open('logins.json', 'r') as file:
+                logins = json.load(file)
+
+        except json.JSONDecodeError:
+            #handle empty logins,json or invalid json
+            logins = []
+
+
+    login_entry = {'website': website, 'username': username, 'note': note}
+    logins.append(login_entry)
+
+    #save list
+    with open('logins.json', 'w') as file:
+        json.dump(logins, file, indent = 4)
 
 
 
+#def find_login_by_website():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#def find_login_by_username():
 
 
 
